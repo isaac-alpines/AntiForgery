7e5d3bac0631f395b37df3a1a0e730f5a95f69f6
Ishak Alpfidan <isaac.alpines@gmail.com>
2021-08-31 16:58:25 +0300

    Created Anti Forgery view and fake test view

---
+++ WebHelpers/Controllers/AntiForgeryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebHelpers.Models;
+
+namespace WebHelpers.Controllers
+{
+    public class AntiForgeryController : Controller
+    {
+        public ActionResult Test()
+        {
+            return View(Database.listData);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Test(int id)
+        {
+            Database.listData.RemoveAt(id);
+
+            return RedirectToAction("test");
+        }
+
+        public ActionResult FakeTest()
+        {
+            return View(Database.listData);
+        }
+    }
+}
---
+++ WebHelpers/Models/Database.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebHelpers.Models
+{
+    public static class Database
+    {
+        public static List<string> listData = new List<string>(new string[] { "apple", "samsung", "xiaomi", "huawei" });  
+    }
+}
